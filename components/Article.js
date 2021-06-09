66a67f5026300d8818a9887c186b2f509f6c846b
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-06-09 00:40:43 -0400

    Completed Entire Assignment

--- components/Article.js
+++ components/Article.js
@@ -85,6 +85,15 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'RANDOM TITLE',
+    date: 'JRANDOM DATE',
+    firstParagraph: `WALKHFAWFJHAWLFKJH  `,
+
+    secondParagraph: `WAFLWAFHJJKDFIQWUFHQWU `,
+
+    thirdParagraph: `QPWUDWQIHDOQ.`
   }
 ];
 // You can read about ES6 modules here: https://exploringjs.com/es6/ch_modules.html#sec_basics-of-es6-modules
